c8e6a1e76154846b5f507827c97b7966e5df5ed6
Matt Ord <matthew.ord1@gmail.com>
2025-06-05 11:25:57 +0000

    Improve comments

--- examples/lobatto.py
+++ examples/lobatto.py
@@ -4,18 +4,23 @@ import numpy as np
 from multiscat.lobatto import LobattoMetadata, get_polynomials
 
 if __name__ == "__main__":
-    # Ok up until around 35 points
+    # An example visualizing the Lobatto polynomials, used to represent
+    # the scattering potential in a Lobatto basis.
+    # The lobatto polynomials are particularly well suited for
+    # representing the scattering potential.
+
+    # TODO: this breaks around 35 points  # noqa: FIX002
     # This is due to an issue with finding the Legendre derivative roots
+    # we need a better aproach!
     lobatto_metadata = LobattoMetadata(35, 2.0)
 
-    fig, ax = plt.subplots()  # type: ignore unknown
     points = np.linspace(
         lobatto_metadata.values[0],
         lobatto_metadata.values[-1],
         10000,
         dtype=np.float64,
     )
-
+    fig, ax = plt.subplots()  # type: ignore unknown
     for polynomial in get_polynomials(lobatto_metadata):
         ax.plot(points, polynomial(points))  # type: ignore unknown
     fig.show()
