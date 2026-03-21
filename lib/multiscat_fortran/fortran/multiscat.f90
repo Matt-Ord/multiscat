06146a0b9fe0e7ae9d33bc64f2e7f43d2360df88
Matthew Ord <matthew.ord1@gmail.com>
2026-03-21 10:40:30 +0000

    return full complex state

--- lib/multiscat_fortran/fortran/multiscat.f90
+++ lib/multiscat_fortran/fortran/multiscat.f90
@@ -19,41 +19,42 @@ contains
 
    subroutine run_scattering_linear_step( &
    & optimization_data, potential_values, perpendicular_kinetic_difference, wave_a, wave_b, &
-   & wave_c, channel_intensity_dense, parallel_kinetic_energy)
+   & wave_c, scattered_state_dense, parallel_kinetic_energy, gmres_info)
       implicit none
       type(OptimizationData), intent(in) :: optimization_data
       complex(dp), intent(in) :: potential_values(:,:,:)
       real(dp), intent(in) :: perpendicular_kinetic_difference(:,:)
       complex(dp), intent(in) :: wave_a(:), wave_b(:), wave_c(:)
-      real(dp), intent(out) :: channel_intensity_dense(:,:)
+      complex(dp), intent(out) :: scattered_state_dense(:,:,:)
       real(dp), intent(in) :: parallel_kinetic_energy(:,:)
+      integer, intent(out) :: gmres_info
       real(dp) :: eps
       integer :: i, j, idx, alloc_status, n_z_points, channel_count
-      real(dp), allocatable :: channel_intensity(:)
+      complex(dp), allocatable :: scattered_state(:,:)
 
       eps = 0.5_dp*(10.0_dp**(-optimization_data%convergence_significant_figures))
       n_z_points = size(potential_values, 3)
       channel_count = size(perpendicular_kinetic_difference, 1) * size(perpendicular_kinetic_difference, 2)
 
-      allocate(channel_intensity(channel_count), stat=alloc_status)
-      if (alloc_status /= 0) error stop 'ERROR: allocation failure (channel_intensity).'
+      allocate(scattered_state(n_z_points,channel_count), stat=alloc_status)
+      if (alloc_status /= 0) error stop 'ERROR: allocation failure (scattered_state).'
 
       call run_preconditioned_gmres ( &
          n_z_points, &
          potential_values, size(potential_values, 1), size(potential_values, 2), &
-         wave_a, wave_b, wave_c, perpendicular_kinetic_difference, channel_intensity, &
-      & parallel_kinetic_energy, eps, optimization_data%gmres_preconditioner_flag &
+         wave_a, wave_b, wave_c, perpendicular_kinetic_difference, scattered_state, &
+      & parallel_kinetic_energy, eps, optimization_data%gmres_preconditioner_flag, gmres_info &
          )
 
       idx = 0
-      do i = 1,size(channel_intensity_dense, 1)
-         do j = 1,size(channel_intensity_dense, 2)
+      do i = 1,size(scattered_state_dense, 1)
+         do j = 1,size(scattered_state_dense, 2)
             idx = idx + 1
-            channel_intensity_dense(i, j) = channel_intensity(idx)
+            scattered_state_dense(i, j, :) = scattered_state(:, idx)
          end do
       end do
 
-      if (allocated(channel_intensity)) deallocate(channel_intensity)
+      if (allocated(scattered_state)) deallocate(scattered_state)
    end subroutine run_scattering_linear_step
 
 end module multiscat_core
